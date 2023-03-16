48e4324f894934a3578018169942f35065fe5005
Eldos <eldos.baktybekov@gmail.com>
2023-03-16 21:37:12 +0600

    :white_check_mark: Add tests for http client config factory (#GI-38)

--- dodo_is_api/connection/http_clients.py
+++ dodo_is_api/connection/http_clients.py
@@ -2,7 +2,11 @@ import contextlib
 
 import httpx
 
-__all__ = ('get_http_client_config',)
+__all__ = (
+    'get_http_client_config',
+    'closing_http_client',
+    'closing_async_httpx_client',
+)
 
 
 def get_http_client_config(*, country_code: str, timeout: int = 60) -> dict:
@@ -19,3 +23,35 @@ def get_http_client_config(*, country_code: str, timeout: int = 60) -> dict:
         'base_url': f'https://api.dodois.io/dodopizza/{country_code}/',
         'timeout': timeout,
     }
+
+
+@contextlib.contextmanager
+def closing_http_client(*, country_code: str) -> httpx.Client:
+    """Sync HTTP client for Dodo IS API connection.
+    Use only with context manager!
+
+    Args:
+        country_code: country code defined in ISO 3166.
+
+    Returns:
+        Pre-configured httpx.Client.
+    """
+    config = get_http_client_config(country_code=country_code)
+    with httpx.Client(**config) as http_client:
+        yield http_client
+
+
+@contextlib.asynccontextmanager
+async def closing_async_httpx_client(*, country_code: str) -> httpx.AsyncClient:
+    """Async HTTP client for Dodo IS API connection.
+    Use only with async context manager!
+
+    Args:
+        country_code: country code defined in ISO 3166.
+
+    Returns:
+        Pre-configured httpx.AsyncClient.
+    """
+    config = get_http_client_config(country_code=country_code)
+    async with httpx.AsyncClient(**config) as http_client:
+        yield http_client
