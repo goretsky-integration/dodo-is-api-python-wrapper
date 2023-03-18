0a8c62415628120ae73e6b0eaad315fe3e503e74
Eldos <eldos.baktybekov@gmail.com>
2023-03-18 16:51:46 +0600

    :sparkles: Add stop sales API calls (#GI-40)

--- dodo_is_api/connection/synchronous.py
+++ dodo_is_api/connection/synchronous.py
@@ -5,6 +5,7 @@ from uuid import UUID
 import httpx
 
 from .base import BaseConnection, concatenate_uuids, raise_for_status
+from ..models import raw as raw_models
 
 __all__ = ('DodoISAPIConnection',)
 
@@ -42,3 +43,60 @@ class DodoISAPIConnection(BaseConnection):
             if response_data['isEndOfListReached']:
                 break
             request_query_params['skip'] += take
+
+    def get_stop_sales_by_ingredients(
+            self,
+            *,
+            from_date: datetime.datetime,
+            to_date: datetime.datetime,
+            units: Iterable[UUID],
+    ) -> list[raw_models.StopSaleByIngredientTypedDict]:
+        url = '/production/stop-sales-ingredients'
+        request_query_params = {
+            'from': from_date.strftime('%Y-%m-%dT%H:%M:%S'),
+            'to': to_date.strftime('%Y-%m-%dT%H:%M:%S'),
+            'units': concatenate_uuids(units),
+        }
+        response = self.__http_client.get(url, params=request_query_params)
+        raise_for_status(response)
+
+        response_data: dict = response.json()
+        return response_data['stopSalesByIngredients']
+
+    def get_stop_sales_by_sales_channels(
+            self,
+            *,
+            from_date: datetime.datetime,
+            to_date: datetime.datetime,
+            units: Iterable[UUID],
+    ) -> list[raw_models.StopSaleBySalesChannelTypedDict]:
+        url = '/production/stop-sales-channels'
+        request_query_params = {
+            'from': from_date.strftime('%Y-%m-%dT%H:%M:%S'),
+            'to': to_date.strftime('%Y-%m-%dT%H:%M:%S'),
+            'units': concatenate_uuids(units),
+        }
+        response = self.__http_client.get(url, params=request_query_params)
+        raise_for_status(response)
+
+        response_data: dict = response.json()
+        return response_data['stopSalesBySalesChannels']
+
+    def get_stop_sales_by_products(
+            self,
+            *,
+            from_date: datetime.datetime,
+            to_date: datetime.datetime,
+            units: Iterable[UUID],
+    ) -> list[raw_models.StopSaleByProductTypedDict]:
+        url = '/production/stop-sales-products'
+        request_query_params = {
+            'from': from_date.strftime('%Y-%m-%dT%H:%M:%S'),
+            'to': to_date.strftime('%Y-%m-%dT%H:%M:%S'),
+            'units': concatenate_uuids(units),
+        }
+        response = self.__http_client.get(url, params=request_query_params)
+        raise_for_status(response)
+
+        response_data: dict = response.json()
+        return response_data['stopSalesByProducts']
