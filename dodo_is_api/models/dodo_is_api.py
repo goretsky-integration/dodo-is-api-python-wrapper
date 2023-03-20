4255aaf19b4246ebe60370ffddaec670fc4e86d3
Eldos <eldos.baktybekov@gmail.com>
2023-03-20 11:11:18 +0600

    :construction: Add `UnitDeliveryStatistics` DTO model (#GI-46)

--- dodo_is_api/models/dodo_is_api.py
+++ dodo_is_api/models/dodo_is_api.py
@@ -11,6 +11,7 @@ __all__ = (
     'StopSaleBySalesChannel',
     'SalesChannel',
     'ChannelStopType',
+    'UnitDeliveryStatistics',
 )
 
 
@@ -64,3 +65,20 @@ class StopSaleByIngredient(StopSale):
 @dataclass(frozen=True, slots=True)
 class StopSaleByProduct(StopSale):
     product_name: str
+
+
+@dataclass(frozen=True, slots=True)
+class UnitDeliveryStatistics:
+    unit_uuid: UUID
+    unit_name: str
+    delivery_sales: int
+    delivery_orders_count: int
+    average_delivery_order_fulfillment_time: int
+    average_cooking_time: int
+    average_heated_shelf_time: int
+    average_order_trip_time: int
+    late_orders_count: int
+    trips_count: int
+    trips_duration: int
+    couriers_shifts_duration: int
+    orders_with_courier_app_count: int
